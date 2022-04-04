dcbf2a714ab79133db599be7d0eee8236b769376
Jiří <satorajirka@seznam.cz>
2022-04-04 18:34:46 +0200

    make progress bars animated

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,3 +1,17 @@
 import makeStickyHeader from './modules/stickyHeader.js';
+import ElementsObserver from './modules/elementsObserver.js';
 
+// STICKY HEADER
 makeStickyHeader(document.querySelector('.intro-section__header'), document.querySelector('.header'), 'header--fixed');
+
+// PROGRESS BARS
+const animatedProgressBars = document.querySelectorAll('.progress-bar--hidden');
+
+const observer = new ElementsObserver(0.9, () => {
+    // if Intersection Observer is not supported, remove CSS class now
+    for (let progressBar of animatedProgressBars) progressBar.classList.remove('progress-bar--hidden');
+});
+
+for (let progressBar of animatedProgressBars) {
+    observer.observe(progressBar, 'progress-bar--hidden', 'removeOnEnter');
+}
