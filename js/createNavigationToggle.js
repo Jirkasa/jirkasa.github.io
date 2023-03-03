b232cc88d01c983d08853e8c358a0fb42ded8c23
Jiří <satorajirka@seznam.cz>
2023-03-03 17:57:31 +0100

    add focus styles

--- js/createNavigationToggle.js
+++ js/createNavigationToggle.js
@@ -6,15 +6,39 @@
  * @param {string} navigationOpenedCSSClass CSS class to add to navigation when navigation is opened.
  */
 function createNavigationToggle(navigationToggleButton, navigation, navigationOpenedButtonCSSClass, navigationOpenedCSSClass) {
+    let opened = false;
+    const links = navigation.querySelectorAll("a");
+
+    const mql = window.matchMedia('(max-width: 46.875em)');
+
+    const updateTabIndices = () => {
+        if (mql.matches && !opened) {
+            for (let link of links) {
+                link.setAttribute("tabindex", -1);
+            }
+        } else {
+            for (let link of links) {
+                link.removeAttribute("tabindex");
+            }
+        }
+    }
+
+    mql.onchange = updateTabIndices;
+    updateTabIndices();
+
     navigationToggleButton.addEventListener("click", () => {
+        opened = !opened;
         navigationToggleButton.classList.toggle(navigationOpenedButtonCSSClass);
         navigation.classList.toggle(navigationOpenedCSSClass);
+        updateTabIndices();
     });
 
     navigation.addEventListener("click", (evt) => {
         if (evt.target.tagName === "A") {
+            opened = false;
             navigationToggleButton.classList.remove(navigationOpenedButtonCSSClass);
             navigation.classList.remove(navigationOpenedCSSClass);
+            updateTabIndices();
         }
     });
 }
